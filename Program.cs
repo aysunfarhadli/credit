cb8a8d7e092d4e96d84ec4bbcc9112d837e7b7fd
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 13:12:05 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ class Program
 {
     static void Main()
     {
-        // İstifadəçidən maaş və kredit məbləğini daxil etməsini istəyirik
-        Console.Write("İllik maaşınızı daxil edin: ");
+        Console.Write("İllik maaşınızı daxil edin");
         double maas = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Kredit məbləğini daxil edin: ");
+        Console.Write("Kredit məbləğini daxil edin");
         double kredit = Convert.ToDouble(Console.ReadLine());
 
-        // Şərti yoxlayırıq
         if (kredit > (maas * 0.6))
         {
             Console.WriteLine("Kredit götürə bilərsiniz!");
