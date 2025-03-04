22b8cef887b9ddc98f1e009bcc36fd251fdd5e2a
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 11:46:58 -0800

    code commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // İstifadəçidən maaş və kredit məbləğini daxil etməsini istəyirik
+        Console.Write("İllik maaşınızı daxil edin: ");
+        double maas = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Kredit məbləğini daxil edin: ");
+        double kredit = Convert.ToDouble(Console.ReadLine());
+
+        // Şərti yoxlayırıq
+        if (kredit > (maas * 0.6))
+        {
+            Console.WriteLine("Kredit götürə bilərsiniz!");
+        }
+        else
+        {
+            Console.WriteLine("Kredit götürə bilməzsiniz!");
+        }
+    }
+}
